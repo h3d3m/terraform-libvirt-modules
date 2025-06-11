2be68e242e9b31367184e6a11927fcaf461afe0e
h3d3m <dubr0vin.n@yandex.ru>
2025-06-11 22:24:09 +0300

    updated versions

--- storage_pool/versions.tf
+++ storage_pool/versions.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = ">= 1.5.0"
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "~> 0.8"
+      version = "~> 0.7.0"
     }
   }
 }
